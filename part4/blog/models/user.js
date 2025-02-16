d9dc2ae4ba603b1b3b85a4f972626dd18c82ccf6
hdahlber <hektor.dahlberg@gmail.com>
2025-02-16 17:35:28 +0200

    4.17 done

--- part4/blog/models/user.js
+++ part4/blog/models/user.js
@@ -9,7 +9,8 @@ const userSchema = new mongoose.Schema({
     },
     name: String,
     passwordhash: String,
-    blogs: [{
+    blogs: [
+        {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Blog'
     }]
