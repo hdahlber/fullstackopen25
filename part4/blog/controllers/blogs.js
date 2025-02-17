36b316669cda107606751ec63f8461dec4b04568
hdahlber <hdahlber@abo.fi>
2025-02-17 20:04:34 +0200

    4.21 done

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,8 +1,5 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const User = require('../models/user')
-const jwt = require('jsonwebtoken')
-const config = require('../utils/config')
 //const logger = require('../utils/logger')
 
 
@@ -22,19 +19,15 @@ blogsRouter.get('/:id', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response,next) => {
-  //logger.info('Received token:', request.token)
-  const decodedToken = jwt.verify(request.token, config.SECRET)
-  //logger.info('Decoded token:', decodedToken)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
-  }
-  const user = await User.findById(decodedToken.id)
-  //logger.info('User found:', user)
-  const blog = new Blog({
-    ...request.body,
-    user: user.id
-  })
-  try{
+  if (!request.token) return response.status(400).json({ error: 'No token found' })
+  if (!request.body.title) return response.status(400).json({ error: 'Title not found' })
+  if (!request.body.url) return response.status(400).json({ error: 'URL not found' })
+  try {
+    const user = request.user
+    const blog = new Blog({
+      ...request.body,
+      user: user.id
+    })
     const savedBlog = await blog.save()
     user.blogs = user.blogs.concat(savedBlog._id)
     await user.save()
@@ -47,11 +40,7 @@ blogsRouter.post('/', async (request, response,next) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  const decodedToken = jwt.verify(request.token, config.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
-  }
-  const user = await User.findById(decodedToken.id)
+  const user = request.user
   const blog = await Blog.findById(request.params.id)
   if ( blog.user.toString() === user.id.toString() ){
     await Blog.findByIdAndDelete(request.params.id)
--- part4/blog/utils/middleware.js
+++ part4/blog/utils/middleware.js
@@ -1,4 +1,6 @@
 const logger = require('./logger')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
 
 const requestLogger = (request, response, next) => {
   logger.info('Method:', request.method)
@@ -17,6 +19,14 @@ const tokenExtractor = (request, response, next) => {
   if (authorization && authorization.startsWith('Bearer ')) {
     request.token = authorization.replace('Bearer ', '')
   }
+  next()
+}
+
+const userExtractor  = async (request, response, next) => {
+  if (request.token) {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    request.user = await User.findById(decodedToken.id)
+  }8
   next()
 }
 
@@ -42,5 +52,6 @@ module.exports = {
   requestLogger,
   unknownEndpoint,
   errorHandler,
-  tokenExtractor
+  tokenExtractor,
+  userExtractor
 }
