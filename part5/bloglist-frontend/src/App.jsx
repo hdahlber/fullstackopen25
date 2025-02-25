1dee9a3f9fad18633655d49ba2e19af7a5b0fa81
hdahlber <hektor.dahlberg@gmail.com>
2025-02-25 11:37:35 +0200

    5.2 done

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -17,12 +17,24 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
       const user = await loginService.login({
         username, password,
       })
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      ) 
       blogService.setToken(user.token)
       setUser(user)
       setUsername('')
@@ -81,7 +93,14 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{`logged in as ${user.username}`}</p>
+      <p>{`logged in as ${user.username}`}
+      <button onClick={() => {
+        window.localStorage.removeItem('loggedBlogappUser')
+        setUser(null)
+      }}>
+        logout
+      </button>
+      </p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
