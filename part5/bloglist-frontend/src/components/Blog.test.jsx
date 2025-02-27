aaa2391004f3866beae4b735a3c2e9e1531dfc70
hdahlber <hektor.dahlberg@gmail.com>
2025-02-27 12:57:06 +0200

    5.14 done

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,7 +1,8 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders content', () => {
+test('renders content', async () =>  {
   const blog = {
     user: 'OneTestToRuleThemAll',
     likes: 3,
@@ -21,4 +22,25 @@ test('renders content', () => {
   expect(likesElement).not.toBeInTheDocument()
   const urlElement = screen.queryByText('lott.com')
   expect(urlElement).not.toBeInTheDocument()
+})
+test('renders details when button clicked', async () =>  {
+  const user = userEvent.setup()
+  const blog = {
+    user: {
+      name: 'The Test',
+      id: 'OneTestToRuleThemAll'
+    },
+    likes: 3,
+    author: 'Tester McTester',
+    title: 'The lord of the Tester',
+    url: 'lott.com'
+  }
+  const { container } = render(<Blog blog={blog} />)
+  const button = screen.getByText('view')
+  await user.click(button)
+  const detailsElement = container.querySelector('.blog-details')
+  expect(detailsElement).toHaveTextContent('lott.com')
+  expect(detailsElement).toHaveTextContent('likes 3')
+  expect(detailsElement).toHaveTextContent('The Test')
+
 })
