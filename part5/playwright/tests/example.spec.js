afb6adf78485b6da51b3181e649fda4e6c1beb8c
hdahlber <hektor.dahlberg@gmail.com>
2025-02-28 17:54:46 +0200

    5.17 done

---
+++ part5/playwright/tests/example.spec.js
@@ -0,0 +1,16 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    const loginForm = await page.locator('form')
+    await expect(loginForm).toBeVisible()
+    const usernameInput = loginForm.locator('input[name="Username"]')
+    await expect(usernameInput).toBeVisible()
+
+
+  })
+})
