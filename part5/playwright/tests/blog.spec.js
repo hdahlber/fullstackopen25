dc122850e563f3d14ec43fcceccb151f398fb549
hdahlber <hektor.dahlberg@gmail.com>
2025-03-09 17:59:59 +0200

    5.20 done

--- part5/playwright/tests/blog.spec.js
+++ part5/playwright/tests/blog.spec.js
@@ -1,17 +1,20 @@
-const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { test, expect, beforeEach,beforeAll, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page, request }) => {
+  beforeEach(async ({ page,request }) => {
     await request.post('http://localhost:3001/api/testing/reset')
     await request.post('http://localhost:3001/api/users', {
       data: {
         name: 'Esko Eskola',
         username: 'esko',
-        password: 'salainen'
-      }
+        password: 'salainen',
+      },
     })
     await page.goto('http://localhost:5173')
   })
+
+    
+  
 
   test('Login form is shown', async ({ page }) => {
     const loginForm = await page.locator('form')
@@ -21,29 +24,29 @@ describe('Blog app', () => {
 
 
   })
-  describe('log in',() =>{
-    test('succeeds with correct credentials', async ({ page }) => {
-      await page.getByRole('button', { name: 'login' }).click()
-      await page.getByTestId('username').fill('esko')
-      await page.getByTestId('password').fill('salainen')
-      await page.getByRole('button', { name: 'login' }).click()
-      await expect(page.getByText('logged in as esko')).toBeVisible()
-    })
-
-    test('fails with wrong credentials', async ({ page }) => {
-      await page.getByRole('button', { name: 'login' }).click()
-      await page.getByTestId('username').fill('wronguser')
-      await page.getByTestId('password').fill('wrongpassword')
-      await page.getByRole('button', { name: 'login' }).click()
-      await expect(page.getByText('logged in as wronguser')).not.toBeVisible()
-      await expect(page.getByText('Wrong credentials')).toBeVisible({timeout: 5000})
-
-      
-    })
+  
+  test('login succeeds with correct credentials', async ({ page }) => {
+    await page.getByRole('button', { name: 'login' })
+    await page.getByTestId('username').fill('esko')
+    await page.getByTestId('password').fill('salainen')
+    await page.getByRole('button', { name: 'login' }).click()
+    await expect(page.getByText('logged in as esko')).toBeVisible()
   })
+
+  test('login fails with wrong credentials', async ({ page }) => {
+    await page.getByRole('button', { name: 'login' })
+    await page.getByTestId('username').fill('wronguser')
+    await page.getByTestId('password').fill('wrongpassword')
+    await page.getByRole('button', { name: 'login' }).click()
+    await expect(page.getByText('logged in as wronguser')).not.toBeVisible()
+    await expect(page.getByText('Wrong credentials')).toBeVisible({timeout: 5000})
+
+    
+  })
+  
   describe('When logged in', () => {
     beforeEach(async ({ page }) => {
-      await page.getByRole('button', { name: 'login' }).click()
+      await page.getByRole('button', { name: 'login' })
       await page.getByTestId('username').fill('esko')
       await page.getByTestId('password').fill('salainen')
       await page.getByRole('button', { name: 'login' }).click()
@@ -58,7 +61,31 @@ describe('Blog app', () => {
       await expect(page.locator('.blog-title').filter({ hasText: 'testerTitle' }).first()).toBeVisible()
 
 
-     
+  })
+})
+  describe('When logged in and a blog', () => {
+    beforeEach(async ({ page }) => {
+      await page.getByRole('button', { name: 'login' })
+      await page.getByTestId('username').fill('esko')
+      await page.getByTestId('password').fill('salainen')
+      await page.getByRole('button', { name: 'login' }).click()
+      await page.getByRole('button', { name: 'new note' }).click()
+      await page.getByPlaceholder('Title').fill('testerTitle')
+      await page.getByPlaceholder('Author').fill('testerAuthor')
+      await page.getByPlaceholder('Url').fill('testerUrl')
+      await page.getByRole('button', { name: 'create a new blog' }).click()
+      await expect(page.locator('.blog-title').filter({ hasText: 'testerTitle' }).first()).toBeVisible()
     })
+
+  test('a blog can be liked', async ({ page }) => {
+    await page.getByRole('button', { name: 'view' }).click()
+    const likesElement = await page.locator('.blog-likes')
+    const likesText = await likesElement.textContent()
+    const likesCountStart = parseInt(likesText.match(/\d+/)[0])
+    await page.getByRole('button', { name: 'like' }).click()
+    await expect(likesElement).toContainText(`likes ${likesCountStart + 1}`)
+  
+    })  
+  
   })
 })
